0ca5ec6b9d01949cfe34fad09db14bf3b61208c1
gpower12345 <63840160+gpower12345@users.noreply.github.com>
2022-12-09 11:17:55 +0100

    updated

--- sport/settings.py
+++ sport/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$fmiu(uiq+l4e&(q3wn!%t5$1o&^4c1j2j7fzaa7g(=wl8*ksw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
